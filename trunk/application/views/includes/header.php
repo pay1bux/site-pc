0499ae51633b9a6c6b8b1934f695d4f4dc9dd5cd
tricasoft@gmail.com <tricasoft@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2011-08-03 18:02:29 +0000

    Prima pagina

--- trunk/application/views/includes/header.php
+++ trunk/application/views/includes/header.php
@@ -1,38 +1,11 @@
 <html>
-<head>
-<title>First Page</title>
-    <link rel='stylesheet' href="<?php echo base_url()?>css/style.css" type="text/css" media="screen" charset="utf-8">
+    <head>
+        <link rel="stylesheet" href="<?php echo FRONTEND_CSS; ?>poartacerului.css" type="text/css"/>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+        <script type="text/javascript" src="<?php echo FRONTEND_JS; ?>fader.js"></script>
+    </head>
+    <body>
+        <div id="topbar"></div>
 
-<div id="header">
-    <div id="logo">
-</div>
-   <div id="top_menu">
-
-</div>
-    <div id="banner">
-    </div>
-    <div id="first_button">
-            <?php echo 'First Button'; ?>
-        </div>
-
-        <div id="second_button">
-            <?php echo 'Second Button'; ?>
-        </div>
-        <div id="third_button">
-            <?php echo 'Third Button'; ?>
-        </div>
-<div id="sub_video">
-    <div id="video">
-        The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,n 45 BC "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, Video 
-        <?php echo 'Video'; ?>
-    </div>
-
-
-<div id="urm_eveniment">
-        The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-            <?php echo 'Urmatorul eveniment'; ?>
-</div>
-</div>
-</div>
-</head>
-<body>
+        <div class="content">
+            <div id="logo"></div>
