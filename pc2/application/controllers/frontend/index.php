26305a7b678b07d9003f68bcc76552768f0a1dd7
mindhunter4andyg <mindhunter4andyg@gmail.com>
2011-08-10 18:00:48 +0000

    Proiect CI 2.0

---
+++ pc2/application/controllers/frontend/index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Index extends CI_Controller {
+
+	
+	function index() {
+//        $this->load->model('devotional_model');
+//        $data['devotional'] = $this->devotional_model->getUltimulDevotional();
+//
+//        $this->load->model('buletin_duminical_model');
+//        $data['buletin'] = $this->buletin_duminical_model->getUltimulBuletin();
+//
+        $data['main_content'] = 'frontend/index/index';
+
+        $this->load->view('frontend/template', $data);
+    }
+}
