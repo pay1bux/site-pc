016bf896b403005b602004fd9f5a5dc0a8448ab7
tricasoft <tricasoft@gmail.com>
2011-11-27 21:29:54 +0000

    Lucru la convertor

--- pc2/convertor.php
+++ pc2/convertor.php
@@ -19,16 +19,20 @@ function resolveArhiva(){
         $titlu = trim($title_parts[0]);
 
         $data_eveniment = trim($title_parts[1]);
-        var_dump($data_eveniment);
+//        var_dump($data_eveniment);
         $data_zi = substr($data_eveniment, 0, 2);
         $data_luna = substr($data_eveniment, 3, 2);
-        $data_an = substr($data_eveniment, 5, 2);
+        $data_an = substr($data_eveniment, 6, 2);
         $data = "20" . $data_an . "-" . $data_luna . "-" . $data_zi;
-
         $data_adaugare = $row['datainsert'];
 
-        $autor = $title_parts[2];
-        $autor = trim(str_replace("Predica ", "", $autor));
+        if (sizeof($title_parts) > 2) {
+            $autor = $title_parts[2];
+            $autor = trim(str_replace("Predica ", "", $autor));
+        }
+        else {
+            $autor = null;
+        }
         var_dump($autor);
 
         $autor_poarta_cerului = "Poarta Cerului";
@@ -37,15 +41,15 @@ function resolveArhiva(){
             $autor_id = insertAutor($autor_poarta_cerului);
         }
 
-        $resurse_id = insertResursa($titlu, 1, null, 1, null, $data, $data_adaugare);
+//        $resurse_id = insertResursa($titlu, 1, null, 1, null, $data, $data_adaugare);
 
         // evenimente: Binecuvantare copii Sarbatoarea Multumirii Botez Nou Testamental Ordinare diaconi Cantata
         // unknown ***** Partea 2
         // despartitori: si / & / &amp;
 
-        if ($autor == '' || strpos($autor, "****") != -1)
+        if ($autor == null || $autor == '' || strpos($autor, "****") != -1)
             continue;
-        $tag_id = insertTag(1, $resurse_id, $autor);
+//        $tag_id = insertTag(1, $resurse_id, $autor);
     }
 }
 
