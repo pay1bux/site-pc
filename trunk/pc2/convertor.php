79ac1516881a8ae8289de70d6e9f1f822e31556e
tricasoft@gmail.com <tricasoft@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2011-11-29 12:11:13 +0000

    Convertor video!!!

--- trunk/pc2/convertor.php
+++ trunk/pc2/convertor.php
@@ -4,12 +4,15 @@ require_once 'functii.php';
 connect();
 select_db($link);
 
-resolveArhiva();
+//resolvePrograme();
+//resolveTineret();
+resolveStudii();
 
-function resolveArhiva(){
+function resolvePrograme(){
     echo "\nArhiva\n";
 
-    $q = "SELECT * FROM pec_videos_list WHERE category_id = 7";
+    // Select programe
+    $q = "SELECT * FROM pec_videos_list WHERE category_id = 7 order by id";
     $r = mysql_query($q);
 
     while ($row = mysql_fetch_assoc($r)) {
@@ -19,7 +22,6 @@ function resolveArhiva(){
         $titlu = trim($title_parts[0]);
 
         $data_eveniment = trim($title_parts[1]);
-//        var_dump($data_eveniment);
         $data_zi = substr($data_eveniment, 0, 2);
         $data_luna = substr($data_eveniment, 3, 2);
         $data_an = substr($data_eveniment, 6, 2);
@@ -29,6 +31,7 @@ function resolveArhiva(){
         if (sizeof($title_parts) > 2) {
             $autor = $title_parts[2];
             $autor = trim(str_replace("Predica ", "", $autor));
+            $autor = trim(str_replace("predica ", "", $autor));
         }
         else {
             $autor = null;
@@ -41,15 +44,277 @@ function resolveArhiva(){
             $autor_id = insertAutor($autor_poarta_cerului);
         }
 
-//        $resurse_id = insertResursa($titlu, 1, null, 1, null, $data, $data_adaugare);
+        $resurse_id = insertResursa($titlu, $autor_id, null, 1, null, $data, $data_adaugare, $row['views']);
 
-        // evenimente: Binecuvantare copii Sarbatoarea Multumirii Botez Nou Testamental Ordinare diaconi Cantata
+        // evenimente: Binecuvantare copii Sarbatoarea Multumirii Sarbatoarea Roadelor Botez Nou Testamental Ordinare diaconi Cantata Nunta
         // unknown ***** Partea 2
         // despartitori: si / & / &amp;
 
-        if ($autor == null || $autor == '' || strpos($autor, "****") != -1)
-            continue;
-//        $tag_id = insertTag(1, $resurse_id, $autor);
+        if ($autor != null && $autor != '' && strpos($autor, "****") === false && strpos($autor, "Partea 2") === false) {
+            if ($autor == 'Binecuvantare copii') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Sarbatoarea Roadelor') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Sarbatoarea Multumirii') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Botez Nou Testamental') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Ordinare diaconi') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Cantata') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif (strpos($autor, "Nunta") !== false) {
+                insertTag(7, $resurse_id, $autor);
+            }
+            else {
+                $multipli = false;
+                if (strpos($autor, " si ") !== false) {
+                    $autori = explode(" si ", $autor);
+                    $multipli = true;
+                }
+                if (strpos($autor, " &amp; ") !== false) {
+                    $autori = explode(" &amp; ", $autor);
+                    $multipli = true;
+                }
+                if (strpos($autor, " & ") !== false) {
+                    $autori = explode(" & ", $autor);
+                    $multipli = true;
+                }
+                if ($multipli) {
+                    foreach ($autori as $aut) {
+                      insertTag(1, $resurse_id, $aut);
+                    }
+                } else {
+                    insertTag(1, $resurse_id, $autor);
+                }
+            }
+        }
+
+        $src = $row['source'];
+        if ($row['description'] != '' && strpos($row['embed_code'], "href") === false) {
+            $src = str_replace('Pentru a descarca programul <a href="', '', $row['description']);
+            $src = str_replace('Pentru a descarca programul <a href=', '', $src);
+            $pos = strpos($src, '"');
+            $src = substr($src, 0, $pos);
+        }
+        insertAttachment($src, $row['embed_code'], 'flv', $resurse_id, $row['source_thumb']);
+    }
+}
+
+function resolveTineret(){
+    echo "\nTineret\n";
+
+    // Select tineret
+    $q = "SELECT * FROM pec_videos_list WHERE category_id = 4 order by id";
+    $r = mysql_query($q);
+
+    while ($row = mysql_fetch_assoc($r)) {
+        $raw_title = $row['title'];
+        $raw_title = str_replace('Sambata 2', 'Sambata - 2', $raw_title);
+        $title_parts = explode("-", $raw_title);
+
+        $titlu = trim($title_parts[0]);
+
+        $data_eveniment = trim($title_parts[1]);
+        $data_zi = substr($data_eveniment, 0, 2);
+        $data_luna = substr($data_eveniment, 3, 2);
+        $data_an = substr($data_eveniment, 6, 2);
+        $data = "20" . $data_an . "-" . $data_luna . "-" . $data_zi;
+        $data_adaugare = $row['datainsert'];
+
+        if (sizeof($title_parts) > 2) {
+            $autor = $title_parts[2];
+            $autor = trim(str_replace("Predica ", "", $autor));
+            $autor = trim(str_replace("predica ", "", $autor));
+            $autor = trim(str_replace("Marturie ", "", $autor));
+        }
+        else {
+            $autor = null;
+        }
+        var_dump($autor);
+
+        $autor_poarta_cerului = "Poarta Cerului";
+        $autor_id = checkExistingAuthor($autor_poarta_cerului);
+        if ($autor_id == -1) {
+            $autor_id = insertAutor($autor_poarta_cerului);
+        }
+
+        $resurse_id = insertResursa($titlu + $data_eveniment, $autor_id, null, 10, null, $data, $data_adaugare, $row['views']);
+
+        // evenimente: Masa Rotunda Marturii Echipa Teen Challange Conferinta Concert
+        // unknown ***** Partea 2
+        // despartitori: si / & / &amp;
+
+        if ($autor != null && $autor != '' && strpos($autor, "****") === false && strpos($autor, "Tineret") === false) {
+            if ($autor == 'Masa Rotunda') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif ($autor == 'Marturii Echipa Teen Challange') {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif (strpos($autor, 'Conferinta') !== false) {
+                insertTag(7, $resurse_id, $autor);
+            }
+            elseif (strpos($autor, 'Concert') !== false) {
+                insertTag(7, $resurse_id, $autor);
+            }
+            else {
+                $multipli = false;
+                if (strpos($autor, " si ") !== false) {
+                    $autori = explode(" si ", $autor);
+                    $multipli = true;
+                }
+                if (strpos($autor, " &amp; ") !== false) {
+                    $autori = explode(" &amp; ", $autor);
+                    $multipli = true;
+                }
+                if (strpos($autor, " & ") !== false) {
+                    $autori = explode(" & ", $autor);
+                    $multipli = true;
+                }
+                if ($multipli) {
+                    foreach ($autori as $aut) {
+                      insertTag(1, $resurse_id, $aut);
+                    }
+                } else {
+                    insertTag(1, $resurse_id, $autor);
+                }
+            }
+        }
+
+        $src = $row['source'];
+        if ($row['description'] != '' && strpos($row['embed_code'], "href") === false) {
+            $src = str_replace('Pentru a descarca programul <a href="', '', $row['description']);
+            $src = str_replace('Pentru a descarca programul <a href=', '', $src);
+            $pos = strpos($src, '"');
+            $src = substr($src, 0, $pos);
+        }
+        insertAttachment($src, $row['embed_code'], 'flv', $resurse_id, $row['source_thumb']);
+    }
+}
+
+function resolveStudii(){
+    echo "\nStudii\n";
+
+    // Select studii cu categorie
+    $q = "SELECT l.*, cat.title as categorie, autor.title as autor
+            FROM pec_videos_list l, pec_videos_nodes cat, pec_videos_categories2 autor
+            WHERE l.id_cat2 = autor.id AND l.id_node = cat.id AND l.category_id = 5 order by l.id";
+    $r = mysql_query($q);
+
+    while ($row = mysql_fetch_assoc($r)) {
+        $data = null;
+        $data_adaugare = $row['datainsert'];
+
+        $titlu = trim($row['title']);
+        var_dump($titlu);
+
+        $autor = $row['autor'];
+        $autor_id = checkExistingAuthor($autor);
+        if ($autor_id == -1) {
+            $autor_id = insertAutor($autor);
+        }
+
+        $categorie = $row['categorie'];
+        $categorie = str_replace('Titlu Studiu: ', '', $categorie);
+        $categorie_id = checkExistingCategorie($categorie);
+        if ($categorie_id == -1) {
+            $categorie_id = insertCategorie($categorie);
+        }
+
+        $resurse_id = insertResursa($titlu, $autor_id, $categorie_id, 2, null, $data, $data_adaugare, $row['views']);
+
+        $src = $row['source'];
+        if ($row['description'] != '' && strpos($row['embed_code'], "href") === false) {
+            $src = str_replace('Pentru a descarca programul <a href="', '', $row['description']);
+            $src = str_replace('Pentru a descarca programul <a href=', '', $src);
+            $pos = strpos($src, '"');
+            $src = substr($src, 0, $pos);
+        }
+        insertAttachment($src, $row['embed_code'], 'flv', $resurse_id, $row['source_thumb']);
+    }
+
+    // Select studii fara categorie
+    $q = "SELECT l.*
+            FROM pec_videos_list l
+            WHERE l.category_id = 5 AND l.title like '%Sambata%' order by l.id";
+    $r = mysql_query($q);
+
+    while ($row = mysql_fetch_assoc($r)) {
+        $data = null;
+        $data_adaugare = $row['datainsert'];
+
+        $titlu = trim($row['title']);
+        var_dump($titlu);
+
+        $autor = '';
+        if (strpos($titlu, "Nelu Filip") !== false) {
+            $autor = "Nelu Filip";
+        }
+        if (strpos($titlu, "Doru Hriscu") !== false) {
+            $autor = "Doru Hriscu";
+        }
+        $autor_id = checkExistingAuthor($autor);
+        if ($autor_id == -1) {
+            $autor_id = insertAutor($autor);
+        }
+
+        $categorie = "Diverse";
+        $categorie_id = checkExistingCategorie($categorie);
+        if ($categorie_id == -1) {
+            $categorie_id = insertCategorie($categorie);
+        }
+
+        $resurse_id = insertResursa($titlu, $autor_id, $categorie_id, 2, null, $data, $data_adaugare, $row['views']);
+
+        $src = $row['source'];
+        if ($row['description'] != '' && strpos($row['embed_code'], "href") === false) {
+            $src = str_replace('Pentru a descarca programul <a href="', '', $row['description']);
+            $src = str_replace('Pentru a descarca programul <a href=', '', $src);
+            $pos = strpos($src, '"');
+            $src = substr($src, 0, $pos);
+        }
+        insertAttachment($src, $row['embed_code'], 'flv', $resurse_id, $row['source_thumb']);
+    }
+}
+function resolveEvenimente(){
+    echo "\nEvenimente\n";
+
+    // Select evenimente
+    $q = "SELECT * FROM pec_videos_list WHERE category_id = 3 order by id";
+    $r = mysql_query($q);
+
+    while ($row = mysql_fetch_assoc($r)) {
+        $data = null;
+        $data_adaugare = $row['datainsert'];
+
+        $titlu = trim($row['title']);
+        var_dump($titlu);
+
+        $autor_poarta_cerului = "Poarta Cerului";
+        $autor_id = checkExistingAuthor($autor_poarta_cerului);
+        if ($autor_id == -1) {
+            $autor_id = insertAutor($autor_poarta_cerului);
+        }
+
+        $resurse_id = insertResursa($titlu, $autor_id, null, 1, null, $data, $data_adaugare, $row['views']);
+
+        insertTag(7, $resurse_id, $titlu);
+
+        $src = $row['source'];
+        if ($row['description'] != '' && strpos($row['embed_code'], "href") === false) {
+            $src = str_replace('Pentru a descarca programul <a href="', '', $row['description']);
+            $src = str_replace('Pentru a descarca programul <a href=', '', $src);
+            $pos = strpos($src, '"');
+            $src = substr($src, 0, $pos);
+        }
+        insertAttachment($src, $row['embed_code'], 'flv', $resurse_id, $row['source_thumb']);
     }
 }
 
--- trunk/pc2/functii.php
+++ trunk/pc2/functii.php
@@ -16,10 +16,10 @@ function select_db($link) {
 	mysql_select_db('pc-nou', $link) or die('Could not select database.');
 }
 
-function insertResursa($titlu, $autor_id, $categorie_id,  $tip_id, $continut, $data, $data_adaugare) {
+function insertResursa($titlu, $autor_id, $categorie_id, $tip_id, $continut, $data, $data_adaugare, $views) {
 
-    $q2 = "INSERT INTO resurse(titlu, autor_id, categorie_id, tip_id, continut, data, data_adaugare)
-	        VALUES('$titlu', '$autor_id', '$categorie_id',  '$tip_id', '$continut', '$data', '$data_adaugare');";
+    $q2 = "INSERT INTO resurse(titlu, autor_id, categorie_id, tip_id, continut, data, data_adaugare, views)
+	        VALUES('$titlu', '$autor_id', '$categorie_id',  '$tip_id', '$continut', '$data', '$data_adaugare', '$views');";
 	$r2 = mysql_query($q2);
 
 	if (!$r2)
@@ -28,13 +28,23 @@ function insertResursa($titlu, $autor_id, $categorie_id,  $tip_id, $continut, $d
 	return mysql_insert_id();
 }
 
+function checkExistingAuthor($autor) {
+	$q = "SELECT id FROM autor WHERE nume = '$autor'";
+	$r = mysql_query($q);
+
+	if (mysql_num_rows($r) == 0)
+	    return -1;
+	$row = mysql_fetch_assoc($r);
+	    return $row['id'];
+}
+
 function insertAutor($nume) {
 
     $q2 = "INSERT INTO autor(nume) VALUES('$nume');";
 	$r2 = mysql_query($q2);
 
 	if (!$r2)
-	die("Error insertion album: " . $nume);
+	die("Error insertion autor: " . $nume);
 
 	return mysql_insert_id();
 }
@@ -49,12 +59,33 @@ function insertTag($tip_tag_id, $resurse_id, $valoare) {
 	return mysql_insert_id();
 }
 
-function checkExistingAuthor($autor) {
-	$q = "SELECT id FROM autor WHERE nume = '$autor'";
+function checkExistingCategorie($categorie) {
+	$q = "SELECT id FROM categorie_resurse WHERE nume = '$categorie'";
 	$r = mysql_query($q);
 
 	if (mysql_num_rows($r) == 0)
 	    return -1;
 	$row = mysql_fetch_assoc($r);
 	    return $row['id'];
+}
+
+function insertCategorie($categorie) {
+
+    $q2 = "INSERT INTO categorie_resurse(nume) VALUES('$categorie');";
+	$r2 = mysql_query($q2);
+
+	if (!$r2)
+	die("Error insertion categorie: " . $categorie);
+
+	return mysql_insert_id();
+}
+
+function insertAttachment($url, $embed, $format, $resurse_id, $thumb) {
+	$q2 = "INSERT INTO attachment (url, embed, format, resurse_id, thumb) VALUES('$url', '$embed', '$format', '$resurse_id', '$thumb');";
+	$r2 = mysql_query($q2);
+
+	if (!$r2)
+	die("Error insertion attachment: " . $embed);
+
+	return mysql_insert_id();
 }
