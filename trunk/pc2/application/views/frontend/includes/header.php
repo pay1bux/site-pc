e65277e9340a5ff6180619d36c108650ee844820
cristif3@gmail.com <cristif3@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2012-02-29 19:44:42 +0000

    nou!

--- trunk/pc2/application/views/frontend/includes/header.php
+++ trunk/pc2/application/views/frontend/includes/header.php
@@ -3,18 +3,18 @@
 	<a id="Logo" href="index.php">&nbsp;</a>
     <div id="TopMenu">
     	<ul>
-        	<li class="i_mt"><a href="<?php echo BASE_URL;?>live">Live</a></li>
-            <li class="i_mt"><a href="<?php echo BASE_URL;?>contact">Contact</a></li>
+        	<li class="i_mt"><a href="<?php echo site_url("live")?>">Live</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("contact")?>">Contact</a></li>
             <li class="nod"><a href="#"><strong>Resurse</strong><span class="i_icon">&nbsp;</span></a>
             	<ul class="submenu_">
-                	<li class="i_smt"><a href="<?php echo BASE_URL;?>arhiva-audio">Resurse audio</a></li>
-                    <li class="i_smt"><a href="<?php echo BASE_URL;?>">Resurse video</a></li>
+                	<li class="i_smt"><a href="<?php echo site_url("arhiva-audio")?>">Resurse audio</a></li>
+                    <li class="i_smt"><a href="#">Resurse video</a></li>
                     <li class="i_smt last"><a href="#">Alte resurse</a></li>
                 </ul>
             </li>
-            <li class="i_mt"><a href="<?php echo BASE_URL;?>devotional">Devotional</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("devotional")?>">Devotional</a></li>
             <li class="i_mt"><a href="#">Gaseste-ti locul</a></li>
-            <li class="i_mt"><a href="<?php echo BASE_URL;?>despre-noi">Despre noi</a></li>
-            <li class="i_mt"><a href="<?php echo BASE_URL;?>">Acasa</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("despre-noi")?>">Despre noi</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("default_controller")?>">Acasa</a></li>
         </ul>
     </div>
