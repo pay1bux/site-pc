991cb4e7f4be5fcca17a5601eaf472242619d961
tricasoft@gmail.com <tricasoft@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2012-06-18 10:10:32 +0000

    adaugat live-ul nou

--- trunk/pc2/application/views/frontend/live/live.php
+++ trunk/pc2/application/views/frontend/live/live.php
@@ -14,7 +14,9 @@ $clear='class="cleardefault"';
     <div class="clearBoth" style="height:10px;"></div>
     <div id="PageContent">
 <div id="continut" style="width: auto;">
-    <object style="margin-top: -10px;" width="940" height="528"> <param name="movie" value="http://www.poartacerului.ro/media/player_live_streaming/player.swf"><param name="flashvars" value="src=http%3A%2F%2Fwww.poartacerului.ro%2Fmedia%2Fplayer_live_streaming%2Fwirecast.f4m&poster=http%3A%2F%2Fwww.poartacerului.ro%2Fmedia%2Fplayer_live_streaming%2Flogo_live.jpg"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"><embed src="http://www.poartacerului.ro/media/player_live_streaming/player.swf" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="940" height="528" flashvars="src=http%3A%2F%2Fwww.poartacerului.ro%2Fmedia%2Fplayer_live_streaming%2Fwirecast.f4m&poster=http%3A%2F%2Fwww.poartacerului.ro%2Fmedia%2Fplayer_live_streaming%2Flogo_live.jpg"  wmode="transparent"></embed></object>
+    <div align="center" id="blcPlayer">
+    </div>
+    <script type="text/javascript" src="http://embed.bisericilive.com/get?cid=poartaceruluiro&w=720&h=404"></script>
 
 
 
