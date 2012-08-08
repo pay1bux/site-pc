43520c76521411b1c8878e1e16c88cf5b52147ee
cristif3 <cristif3@gmail.com>
2012-08-08 12:43:59 +0000

    pagina de contact (buna, cu harta)

--- pc2/application/views/frontend/contact/contact.php
+++ pc2/application/views/frontend/contact/contact.php
@@ -16,8 +16,8 @@ $buton='class="submit"';
 
  </div>
 
-     <div id="continut">
-
+     <div id="continut" style="width: 940px;">
+<div style="float: left; width: 480px;">
          <div class="p_text">
          <div class="i_title">Contact</div>
          <div class="i_details">
@@ -52,20 +52,32 @@ $buton='class="submit"';
 
         </table>
          </div>
-             <div class="clearBoth"></div>
+
    </div>
-          <div class="clearBoth"></div>
+
 
 
          </div>
  </div>
 
 
- <div id="right">
-     <div class="item" style="background-image: url(<?php echo IMAGES_PATH; ?>right/1.png)"></div>
-     <div class="item" style="background-image: url(<?php echo IMAGES_PATH; ?>right/2.png)"></div>
-     <div class="item" style="background-image: url(<?php echo IMAGES_PATH; ?>right/3.png)"></div>
- </div>
+         <div style="float:left; margin-top: 10px;">
+             <div class="p_text">
+                 <div class="i_title">Biserica Penticostala Poarta Cerului</div>
+                 <div class="i_details">
+                     <p class="mic ">Str.Banul Maracine nr.25</p>
+                     <p class="mic ">Timisoara</p>
+                     <p class="mic ">0256-472002</p>
+                     <br/>
+
+                     </div>
+                 </div>
+
+            <img class="rama" src="<?php echo IMAGES_PATH; ?>contact/harta.png" />
+
+         </div>
+     </div>
+
 
    <div class="clearBoth"></div>
 </div>
