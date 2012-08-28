58b73a7fd73e95e1753d13056cb33e3a3e412efb
tricasoft@gmail.com <tricasoft@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2012-08-28 06:32:12 +0000

    Adaugat ruta "acasa" sa nu mai apara "default-controller"

--- trunk/pc2/application/views/frontend/includes/header.php
+++ trunk/pc2/application/views/frontend/includes/header.php
@@ -1,6 +1,6 @@
 <div id="GreenBar"></div>
 <div id="SiteContent">
-	<a id="Logo" href="<?php echo site_url("default_controller")?>">&nbsp;</a>
+	<a id="Logo" href="<?php echo site_url("acasa")?>">&nbsp;</a>
     <div id="TopMenu">
     	<ul>
         	<li class="i_mt"><a href="<?php echo site_url("live2")?>">Live</a></li>
@@ -15,7 +15,7 @@
             <li class="i_mt"><a href="<?php echo site_url("devotional")?>">Devotional</a></li>
             <li class="i_mt"><a href="<?php echo site_url("gaseste")?>">Gaseste-ti locul</a></li>
             <li class="i_mt"><a href="<?php echo site_url("despre-noi")?>">Despre noi</a></li>
-            <li class="i_mt"><a href="<?php echo site_url("default_controller")?>">Acasa</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("acasa")?>">Acasa</a></li>
         </ul>
            <div class="FooterLine" style="width: 600px; margin-top : 35px; margin-left: 63px;"></div>
     </div>
