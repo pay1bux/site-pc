3c8abce205632adaa36ea2847ed4d4a87efe3863
tricasoft <tricasoft@gmail.com>
2012-10-03 09:15:54 +0000

    Fix la conversia si adaugarea de buletine - trebuie ID autor > 1

--- pc2/application/controllers/admin/buletin.php
+++ pc2/application/controllers/admin/buletin.php
@@ -28,7 +28,7 @@ class Buletin extends CI_Controller {
             $input = array(
                 'titlu' => $postdata['titlu'],
                 'data' => $postdata['data'],
-                'autor_id' => 0,
+                'autor_id' => 1,
                 'categorie_id' => 0,
                 'tip_id' => $tip_buletin->id
             );
