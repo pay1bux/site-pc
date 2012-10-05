6ec9f98d7b08c159b311bddea41829afb7907f95
tricasoft@gmail.com <tricasoft@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2012-10-05 11:32:28 +0000

    corectat greseala gramaticala

--- trunk/pc2/application/views/frontend/includes/header.php
+++ trunk/pc2/application/views/frontend/includes/header.php
@@ -10,7 +10,7 @@
                 	<li class="i_smt"><a href="<?php echo site_url("arhiva-audio")?>">Resurse audio</a></li>
                     <li class="i_smt"><a href="<?php echo site_url("arhiva-video")?>">Resurse video</a></li>
                     <li class="i_smt"><a href="<?php echo site_url("buletin-duminical")?>">Buletin duminical</a></li>
-                    <li class="i_smt last"><a href="<?php echo site_url("cereri-rugaciune")?>">Cereri de ragaciune</a></li>
+                    <li class="i_smt last"><a href="<?php echo site_url("cereri-rugaciune")?>">Cereri de rugaciune</a></li>
                 </ul>
             </li>
             <li class="i_mt"><a href="<?php echo site_url("devotional")?>">Devotional</a></li>
