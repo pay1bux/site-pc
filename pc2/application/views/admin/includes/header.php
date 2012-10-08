3ab2eb605144b894e08a4b53baa701b4edb9b0a3
tricasoft <tricasoft@gmail.com>
2012-10-08 20:34:15 +0000

    header modificat si pentru partea de administrare

--- pc2/application/views/admin/includes/header.php
+++ pc2/application/views/admin/includes/header.php
@@ -1,25 +1,23 @@
 <div id="GreenBar"></div>
 <div id="SiteContent">
-	<a id="Logo" href="<?php echo site_url("default_controller")?>">&nbsp;</a>
+    <a id="Logo" href="<?php echo site_url("acasa")?>">&nbsp;</a>
     <div id="TopMenu">
         <ul>
             <li class="i_mt"><a href="<?php echo site_url("live2")?>">Live</a></li>
             <li class="i_mt"><a href="<?php echo site_url("contact")?>">Contact</a></li>
             <li class="nod"><a href="#"><strong>Resurse</strong><span class="i_icon">&nbsp;</span></a>
                 <ul class="submenu_">
+                    <li class="i_smt"><a href="<?php echo site_url("arhiva-video")?>">Resurse video</a></li>
                     <li class="i_smt"><a href="<?php echo site_url("arhiva-audio")?>">Resurse audio</a></li>
-                    <li class="i_smt"><a href="#">Resurse video</a></li>
                     <li class="i_smt"><a href="<?php echo site_url("buletin-duminical")?>">Buletin duminical</a></li>
-                    <li class="i_smt last"><a href="<?php echo site_url("cereri-rugaciune")?>">Cereri de ragaciune</a></li>
+                    <li class="i_smt last"><a href="<?php echo site_url("cereri-rugaciune")?>">Cereri de rugăciune</a></li>
                 </ul>
             </li>
-            <li class="i_mt"><a href="<?php echo site_url("devotional")?>">Devotional</a></li>
-            <li class="i_mt"><a href="<?php echo site_url("gaseste")?>">Gaseste-ti locul</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("devotional")?>">Devoțional</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("gaseste")?>">Găsește-ți locul</a></li>
             <li class="i_mt"><a href="<?php echo site_url("despre-noi")?>">Despre noi</a></li>
-            <li class="i_mt"><a href="<?php echo site_url("acasa")?>">Acasa</a></li>
+            <li class="i_mt"><a href="<?php echo site_url("acasa")?>">Acasă</a></li>
+
         </ul>
         <div class="FooterLine" style="width: 600px; margin-top : 35px; margin-left: 63px;"></div>
     </div>
-
-
-
