4a709dd4d88b2e539c1f91a62ab8fd4f4064f365
cristif3@gmail.com <cristif3@gmail.com@4113ced8-b9ab-2c63-fa17-e24538192174>
2013-12-10 11:32:28 +0000

    updated radio

--- trunk/pc2/application/controllers/frontend/radio.php
+++ trunk/pc2/application/controllers/frontend/radio.php
@@ -7,7 +7,7 @@ class Radio extends CI_Controller {
 //        $data['main_content'] = 'frontend/radio/radio';
 //        $data['page_title'] = 'Radio Poarta Cerului - Biserica Penticostala Poarta Cerului, Timisoara';
 
-        $this->load->view('frontend/radio');
+        $this->load->view('frontend/radio/radio');
     }
 
 }
