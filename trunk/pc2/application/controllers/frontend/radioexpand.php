a16e6576161ea3a106cd37e081258836cbbf30e5
tricasoft <tricasoft@4113ced8-b9ab-2c63-fa17-e24538192174>
2015-05-01 06:05:38 +0000

    Pagina cu radio expand

---
+++ trunk/pc2/application/controllers/frontend/radioexpand.php
@@ -0,0 +1,13 @@
+<?php
+
+class Radioexpand extends CI_Controller {
+
+	function index() {
+
+//        $data['main_content'] = 'frontend/radio/radio';
+//        $data['page_title'] = 'Radio Poarta Cerului - Biserica Penticostala Poarta Cerului, Timisoara';
+
+        $this->load->view('frontend/radio/radioexpand');
+    }
+
+}
