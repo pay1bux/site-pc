1e7cba5c7bf2cdc570ec6a563669cd107bbc5ce8
tricasoft <tricasoft@4113ced8-b9ab-2c63-fa17-e24538192174>
2015-06-08 22:05:50 +0000

    Design radio expand

--- pc2/application/views/frontend/radio/radioexpand.php
+++ pc2/application/views/frontend/radio/radioexpand.php
@@ -5,11 +5,12 @@
     <title>Radio</title>
     <script src="<?php echo JS_PATH; ?>/jquery-1.4.4.min.js" type="text/javascript"></script>
     <script src="<?php echo JS_PATH; ?>/jquery.showinfo.js" type="text/javascript"></script>
-<!--    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>-->
+    <script src="<?php echo JS_PATH; ?>/jqModal.js" type="text/javascript"></script>
+    <script src="<?php echo JS_PATH; ?>jquery.jplayer.min.js" type="text/javascript"></script>
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?php echo JS_PATH; ?>jquery.jplayer.min.js"></script>
-    <link href="<?php echo CSS_PATH; ?>/airtime-widgets.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo CSS_PATH; ?>jplayer.radio.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo CSS_PATH; ?>airtime-widgets.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo CSS_PATH; ?>radio-expand.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo CSS_PATH; ?>poartacerului.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript">
         //<![CDATA[
@@ -18,7 +19,7 @@
                 ready: function (event) {
                     $(this).jPlayer("setMedia", {
                         mp3:"http://89.238.218.26:8000/airtime_128"
-                    });
+                    }).jPlayer("play");;
                 },
                 swfPath: "static/js",
                 supplied: "mp3",
@@ -31,66 +32,81 @@
     </script>
     <script>
         $(document).ready(function() {
-            $("#headerLiveTrackHolder").airtimeLiveTrackInfo({
-                sourceDomain: "https://89.238.218.26:600/",
+            $("#radioPlayer").airtimeLiveTrackInfo({
+                sourceDomain: "http://89.238.218.26:6010/",
                 text: {onAirNow:"", offline:"Offline", current:"Acum", next:"Urmeaza"},
                 updatePeriod: 20 //seconds
             });
             $("#onAirToday").airtimeShowSchedule({
-                sourceDomain: "https://89.238.218.26:600/",
-                text: {onAirToday:"Programul de azi"},
+                sourceDomain: "http://89.238.218.26:6010/",
                 updatePeriod: 20, //seconds
                 showLimit: 10
             });
+
+            //scrollText($('#status-current-show'), $("#liveTrackHolder"));
+            scrollText($('#status-current-track'), $("#liveTrackHolder"));
+            scrollText($('#status-next-track'), $("#nextTrackHolder"));
+
+            $('#dialog').jqm();
         });
     </script>
 </head>
 <body>
-<div id="radioBox">
-    <div class="top"></div>
-    <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-    <div id="jp_container_1" class="jp-audio">
-        <div class="jp-type-single">
-            <div class="jp-gui jp-interface">
-                <div id="player">
-                    <ul class="jp-controls">
-                        <li><a href="javascript:;" class="jp-mute" tabindex="1" title="mute">mute</a></li>
-                        <li><a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute">unmute</a></li>
-                        <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
-                    </ul>
-                    <div class="jp-volume-bar">
-                        <div class="jp-volume-bar-value"></div>
-                    </div>
-                </div>
-            </div>
-            <div id="playerInfo">
-                <div class="jp-gui jp-interface">
-                    <ul class="jp-controls" style="width: 73px; float: left;">
-                        <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
-                        <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+<div id="radioBox" class="radioBox">
 
-                    </ul>
-                    <div class="jp-title" style="float:left;">
-                        <ul>
-                            <li id="headerLiveTrackHolder">RADIO OFFLINE</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="clearLeft"></div>
-                <div class="jp-no-solution">
-                    <span>Update Required</span>
-                    To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
-                </div>
+    <div id="jquery_jplayer_1"></div>
+    <div id="jp_container_1" class="radioPower">
+        <a href="javascript:;" class="jp-mute" tabindex="1" title="mute"><div class="radioMute"></div></a>
+        <a href="javascript:;" class="jp-unmute" tabindex="1" title="unmute"><div class="radioUnmute"></div></a>
+
+        <a id="showProgramPopup" href="#" class="jqModal"><div class="showProgramPopup"></div></a>
+
+        <a href="javascript:;" class="jp-play" tabindex="1"><div class="radioPlay"></div></a>
+        <a href="javascript:;" class="jp-pause" tabindex="1"><div class="radioPause"></div></a>
+    </div>
+
+    <div class="radioInfo">
+        <div class="radioTitle">
+            <br/>
+            Radio Poarta Cerului
+        </div>
+        <div style="height: 15%"></div>
+        <div id="radioPlayer" class="radioPlayer">
+            <div id="liveTrackHolder" class="liveTrackHolder">
+                <table>
+                    <tr><td><div id='status-current-show' class='currentShowName'><br></div></td></tr>
+                    <tr><td><div id='status-current-track' class='currentTrackName'><br></div></td></tr>
+                </table>
+                <div class='currentInfoLabel'>RULEAZA ACUM</div>
+            </div>
+            <div class="radioSeparator">
+
+            </div>
+            <div id="nextTrackHolder" class="nextTrackHolder">
+                <table>
+                    <tr><td><div style='padding: 3px'><br/></div></td></tr>
+                    <tr><td><div id='status-next-track' class='nextTrackName'><br/></div></td></tr>
+                </table>
+                <div class='nextInfoLabel'>URMEAZA</div>
             </div>
         </div>
+        <div style="height: 10%"></div>
     </div>
 </div>
-<br/>
-<!--<div id="headerLiveTrackHolder" style="border: 1px solid #999999; padding: 10px;"></div>-->
-<br/>
-<div id="onAirToday"></div>
-<br/>
-<br/>
+
+<div id="dialog" class="programModal">
+    <div class="programTitle" align="center">
+        <table style="width:100%; height:100%;">
+            <tr>
+                <td style="vertical-align:middle;text-align:center;">
+                    Program
+                </td>
+            </tr>
+        </table></div>
+    <div class="jqmdBC">
+        <div id="onAirToday"></div>
+    </div>
+</div>
 
 </body>
 </html>
